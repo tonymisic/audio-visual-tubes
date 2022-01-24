55e915f139a7d9e2a5d13c1c46b22262e8615baf
tonymisic <tony.misic@ryerson.ca>
2022-01-24 12:57:02 -0500

    model changes

--- model.py
+++ model.py
@@ -17,13 +17,15 @@ class FullModel(nn.Module):
         self.audnet = base_models.resnet18(modal='audio')
         self.avgpool = nn.AdaptiveMaxPool2d((1, 1))
         self.vidnet.layer4.register_forward_hook(get_activation('layer4'))
-        
+        self.attention = nn.MultiheadAttention(512, 1)
     def forward(self, video, audio):
         # audio editing
         B = audio.shape[0]
         aud = self.audnet(audio)
         aud = self.avgpool(aud).view(B,-1)
-        aud = nn.functional.normalize(aud, dim=1)
+        aud = nn.functional.normalize(aud, dim=1) # batch_size, 512
         # video editing
         _ = self.vidnet(video)
-        vid = activation['layer4']
+        vid = activation['layer4']  # batch_size, frames, 14, 14, 512
+        attn_output, attn_output_weights = self.attention(aud, vid.permute([1,0,2]), vid.permute([1,0,2]))
+        return attn_output
